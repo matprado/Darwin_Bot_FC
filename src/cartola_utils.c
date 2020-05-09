ed3c0dca02bdf228c49afe9e7cc548b93f2e5adb
Mateus Prado <mateus.prado@usp.br>
2020-05-08 23:49:04 -0300

    Ajeitando arquivo csv e começando leitura

--- src/cartola_utils.c
+++ src/cartola_utils.c
@@ -2,46 +2,116 @@
 
 struct rodada{
     int id;
-    Atleta *goleiro[MAX_GOLEIROS];
-    Atleta *lateral[MAX_LATERAIS];
-    Atleta *zagueiro[MAX_ZAGUEIROS];
-    Atleta *meia[MAX_MEIAS];
-    Atleta *atacante[MAX_ATACANTES];
-    Atleta *tecnico[MAX_TECNICO];
+    Atleta *gol[MAX_GOLEIROS];
+    int n_gol;
+    Atleta *lat[MAX_LATERAIS];
+    int n_lat;
+    Atleta *zag[MAX_ZAGUEIROS];
+    int n_zag;
+    Atleta *mei[MAX_MEIAS];
+    int n_mei;
+    Atleta *ata[MAX_ATACANTES];
+    int n_ata;
+    Atleta *tec[MAX_TECNICO];
+    int n_tec;
 };
 
 struct atleta{
     char nome[100];
     int id;
-    int rodada;
     int clube_id; //tem os ids dos clubes
-    int status;
-    /*0 -> provável
-    1 -> contundido
-    2 -> nulo*/
-    float pontos;
-    float preco;
-    float valorizacao;
-    float media;
     char nome_clube[50];
+    //Os seguintes dados são vetores de MAX_RODADAS posições pois representam valores entre as rodadas 0 e 38;
+    //OBS: rodada 0 são os dados iniciais dos atletas antes da primeira rodada
+    /*
+    Tipos de status:
+    'P' -> provável
+    'C' -> contundido
+    'N' -> nulo
+    */
+    char status[MAX_RODADAS];
+    int rodada[MAX_RODADAS]; //vetor de "boolean" para indicar se o atleta participou da rodada x ou não
+    float pontos[MAX_RODADAS];
+    float preco[MAX_RODADAS];
+    float valorizacao[MAX_RODADAS];
+    float media[MAX_RODADAS];
     //dados do jogo(acumulativos com as rodadas):
     //obs: no csv daqui pra frente pode estar em qualquer ordem
-    int fs; //faltas sofridas: +0.5
-    int rb; //roubadas de bola: +1.5
-    int pe; //passes errados: -0.3
-    int fc; //faltas cometidas: -0.5 
-    int g; //gols: +8.0
-    int ff; //finalizações para fora: +0.8
-    int ft; //finalizações na trave: +3.0
-    int fd; //finalizações defendidas: +1.2
-    int dd; //defesas difíceis: +3.0
-    int gs; //gols sofridos: -2.0
-    int sg; //jogos sem sofrer gol: +5.0
-    int a; //assistências: +5.0
-    int ca; //cartões amarelos: -2.0
-    int i; //impedimentos: -0.5
-    int cv; //cartões vermelhos: -5.0
-    int pp; //pênatils perdidos: -4.0
-    int gc; //gols contra: -5.0
-    int dp; //defesa de pênalti: +7.0
+    int fs[MAX_RODADAS]; //faltas sofridas: +0.5
+    int rb[MAX_RODADAS]; //roubadas de bola: +1.5
+    int pe[MAX_RODADAS]; //passes errados: -0.3
+    int fc[MAX_RODADAS]; //faltas cometidas: -0.5 
+    int g[MAX_RODADAS]; //gols: +8.0
+    int ff[MAX_RODADAS]; //finalizações para fora: +0.8
+    int ft[MAX_RODADAS]; //finalizações na trave: +3.0
+    int fd[MAX_RODADAS]; //finalizações defendidas: +1.2
+    int dd[MAX_RODADAS]; //defesas difíceis: +3.0
+    int gs[MAX_RODADAS]; //gols sofridos: -2.0
+    int sg[MAX_RODADAS]; //jogos sem sofrer gol: +5.0
+    int a[MAX_RODADAS]; //assistências: +5.0
+    int ca[MAX_RODADAS]; //cartões amarelos: -2.0
+    int i[MAX_RODADAS]; //impedimentos: -0.5
+    int cv[MAX_RODADAS]; //cartões vermelhos: -5.0
+    int pp[MAX_RODADAS]; //pênatils perdidos: -4.0
+    int gc[MAX_RODADAS]; //gols contra: -5.0
+    int dp[MAX_RODADAS]; //defesa de pênalti: +7.0
 };
+
+
+Rodada *rodadas; //Vetor de rodadas do campeonato
+Atleta *atletas[MAX_ATLETAS]; //Vetor com todos os atletas do jogo
+
+
+void inicializa_rodadas(){
+
+    
+
+        rodadas[i].id = i;
+
+        rodadas[i].n_gol = 0;
+        for(int j=0; i<MAX_GOLEIROS; i++)
+            rodadas[i].gol[j] = NULL;
+
+
+    }
+
+}
+
+int ler_csv_campeonato(char *nomeCsv){
+
+    FILE *csv = fopen(nomeCsv, "r");
+    if(!csv){
+        printf("ERRO! FALHA AO ABRIR ARQUIVO CSV!\n");
+        return 0;
+    }
+
+    //inicializa todas as rodadas
+    rodadas = (Rodada *) calloc(MAX_RODADAS, sizeof(Rodada));
+    //LEMBRA DE LIMPAR ESSA PORRA DEPOIS CARALHO
+    
+    for(int i=0; i<MAX_RODADAS; i++){
+
+        rodadas[i].id = 0;
+
+    }
+
+    //pula a primeira linha do csv, header
+    //LEMBRAR DE GARANTIR QUE O CSV TEM QUE TER TERMINAÇÃO DE ARQUIVO PADRÃO LF! (SEM \r)
+    fscanf("%[^\n]");
+
+    /*PARA ESSA FUNÇÃO, EU CONSIDERO QUE O CSV ESTÁ ORGANIZADO DA SEGUINTE FORMA:
+    -ORDENADO PRIMEIRAMENTE PELO ID DO ATLETA
+    -ORDENADO SEGUNDAMENTE PELA RODADA
+    */
+    while(fscanf("%*c")){
+        
+        fseek(csv, -1, SEEK_CUR);
+
+
+    }
+
+    fclose(csv);
+    return 1;
+
+}
+
--- src/manipulacao_dados/2019/csv_utils.py
+++ src/manipulacao_dados/2019/csv_utils.py
@@ -1,12 +1,28 @@
 import csv
 
+
+# inicio-0.csv change to 1 in atletas.rodada_id
+r = csv.reader(open('inicio-0.csv'))  # Here your csv file
+lines = list(r)
+for i in range(1, len(lines)):
+    lines[i][6] = '0'
+writer = csv.writer(open('inicio-0.csv', 'w'))
+writer.writerows(lines)
+
+
 # Merge various .csv files
-files = ['rodada-1.csv', 'rodada-2.csv', 'rodada-3.csv', 'rodada-4.csv', 'rodada-5.csv', 'rodada-6.csv', 'rodada-7.csv', 'rodada-8.csv', 'rodada-9.csv', 'rodada-10.csv', 'rodada-11.csv', 'rodada-12.csv', 'rodada-13.csv', 'rodada-14.csv', 'rodada-15.csv', 'rodada-16.csv', 'rodada-17.csv', 'rodada-18.csv', 'rodada-19.csv',
+files = ['inicio-0.csv', 'rodada-1.csv', 'rodada-2.csv', 'rodada-3.csv', 'rodada-4.csv', 'rodada-5.csv', 'rodada-6.csv', 'rodada-7.csv', 'rodada-8.csv', 'rodada-9.csv', 'rodada-10.csv', 'rodada-11.csv', 'rodada-12.csv', 'rodada-13.csv', 'rodada-14.csv', 'rodada-15.csv', 'rodada-16.csv', 'rodada-17.csv', 'rodada-18.csv', 'rodada-19.csv',
          'rodada-20.csv', 'rodada-21.csv', 'rodada-22.csv', 'rodada-23.csv', 'rodada-24.csv', 'rodada-25.csv', 'rodada-26.csv', 'rodada-27.csv', 'rodada-28.csv', 'rodada-29.csv', 'rodada-30.csv', 'rodada-31.csv', 'rodada-32.csv', 'rodada-33.csv', 'rodada-34.csv', 'rodada-35.csv', 'rodada-36.csv', 'rodada-37.csv', 'rodada-38.csv']
+
+ini_headers = ['', 'atletas.nome', 'atletas.slug', 'atletas.apelido', 'atletas.foto', 'atletas.atleta_id', 'atletas.rodada_id', 'atletas.clube_id', 'atletas.posicao_id', 'atletas.status_id', 'atletas.pontos_num',
+               'atletas.preco_num', 'atletas.variacao_num', 'atletas.media_num', 'atletas.clube.id.full.name']
+
 final_headers = ['', 'atletas.nome', 'atletas.slug', 'atletas.apelido', 'atletas.foto', 'atletas.atleta_id', 'atletas.rodada_id', 'atletas.clube_id', 'atletas.posicao_id', 'atletas.status_id', 'atletas.pontos_num',
                  'atletas.preco_num', 'atletas.variacao_num', 'atletas.media_num', 'atletas.clube.id.full.name', 'FS', 'RB', 'PE', 'FC', 'G', 'FF', 'FT', 'FD', 'DD', 'GS', 'SG', 'A', 'CA', 'I', 'CV', 'PP', 'GC', 'DP']
 header = set()
 header.add(tuple(h for h in final_headers))
+
+ini = True
 
 merged_rows = set()
 for f in files:
@@ -14,21 +30,29 @@ for f in files:
         csvreader = csv.reader(csv_in, delimiter=',')
         headers = dict((h, i) for i, h in enumerate(next(csvreader)))
         for row in csvreader:
-            merged_rows.add(tuple(row[headers[x]] for x in final_headers))
+            if(ini):
+                merged_rows.add(tuple(row[headers[x]] for x in ini_headers))
+            else:
+                merged_rows.add(tuple(row[headers[x]] for x in final_headers))
+        ini = False
 
-with open('3.csv', 'w') as csv_out:
+
+with open('dados_2019.csv', 'w') as csv_out:
     csvwriter = csv.writer(csv_out, delimiter=',')
     csvwriter.writerows(header)
     csvwriter.writerows(merged_rows)
 
 
 # Sort .csv file
-with open('3.csv', newline='') as csvfile:
+with open('dados_2019.csv', newline='') as csvfile:
     spamreader = csv.DictReader(csvfile, delimiter=",")
-    sortedlist = sorted(spamreader, key=lambda row: (row['atletas.apelido'], row['atletas.rodada_id']), reverse=False)
+    sortedlist = sorted(spamreader, key=lambda row: (row['atletas.posicao_id'], int(row['atletas.clube_id']), int(row['atletas.atleta_id']), int(row['atletas.rodada_id'])), reverse=False)
 
-with open('dados.csv', 'w') as f:
-    writer = csv.DictWriter(f, fieldnames=final_headers)
+final_headers_order = ['atletas.posicao_id', 'atletas.clube_id', 'atletas.atleta_id','atletas.rodada_id', 'atletas.nome', 'atletas.slug', 'atletas.apelido', 'atletas.foto', 'atletas.status_id', 'atletas.pontos_num',
+                 'atletas.preco_num', 'atletas.variacao_num', 'atletas.media_num', 'atletas.clube.id.full.name', 'FS', 'RB', 'PE', 'FC', 'G', 'FF', 'FT', 'FD', 'DD', 'GS', 'SG', 'A', 'CA', 'I', 'CV', 'PP', 'GC', 'DP', '']
+
+with open('dados_2019.csv', 'w') as f:
+    writer = csv.DictWriter(f, fieldnames=final_headers_order)
     writer.writeheader()
     for row in sortedlist:
         writer.writerow(row)
